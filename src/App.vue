c4f671be88babd104c1efb06b478b49b83f5bfb8
Javier Dominguez II <Javi7057@gmail.com>
2022-08-06 12:51:51 -0500

    added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@ import { RouterLink, RouterView } from "vue-router";
       <nav>
         <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
       </nav>
+      <div>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </div>
   </header>
 
