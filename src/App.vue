8f02a734078ad99064e9506a77544d0b38b9c5c7
Javier Dominguez II <Javi7057@gmail.com>
2022-08-06 13:16:30 -0500

    initial Navbar setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+// import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
-      </nav>
-      <div>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    </div>
-  </header>
+  <Navbar />
 
   <RouterView />
 </template>
