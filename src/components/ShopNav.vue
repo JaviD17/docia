8e58f8f5e35032420857f54ae2ad05446b84e921
Javier Dominguez II <Javi7057@gmail.com>
2022-08-19 21:08:31 -0500

    added ShopNav.vue

---
+++ src/components/ShopNav.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { ref } from "vue";
+
+const price = ref(false);
+const release = ref(false);
+</script>
+
+<template>
+  <div class="flex justify-between my-16 mx-6">
+    <div class="">
+      <button class="btn btn-circle btn-sm">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="-4.5 -4.5 24 24"
+          width="24"
+          fill="currentColor"
+        >
+          <path
+            d="M8.9 6.9v-5a1 1 0 1 0-2 0v5h-5a1 1 0 1 0 0 2h5v5a1 1 0 1 0 2 0v-5h5a1 1 0 1 0 0-2h-5z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+    <div class="">
+      <div class="dropdown dropdown-hover dropdown-end">
+        <label tabindex="0" class="btn m-1">
+          <span>Sort</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="-5 -8 24 24"
+            width="24"
+            fill="currentColor"
+          >
+            <path
+              d="M7.071 5.314l4.95-4.95a1 1 0 1 1 1.414 1.414L7.778 7.435a1 1 0 0 1-1.414 0L.707 1.778A1 1 0 1 1 2.121.364l4.95 4.95z"
+            ></path>
+          </svg>
+        </label>
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a>By Collection</a></li>
+          <li><a>By Color</a></li>
+        </ul>
+      </div>
+      <div class="dropdown dropdown-hover dropdown-end">
+        <label tabindex="0" class="btn m-1"
+          ><span>Filter</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="-5 -8 24 24"
+            width="24"
+            fill="currentColor"
+          >
+            <path
+              d="M7.071 5.314l4.95-4.95a1 1 0 1 1 1.414 1.414L7.778 7.435a1 1 0 0 1-1.414 0L.707 1.778A1 1 0 1 1 2.121.364l4.95 4.95z"
+            ></path>
+          </svg>
+        </label>
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <div
+              class="dropdown dropdown-left"
+              @mouseenter="price = true"
+              @mouseleave="price = false"
+            >
+              <label tabindex="1" class="">Price</label>
+              <ul
+                v-if="price"
+                tabindex="1"
+                class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li><a>$ - $$$</a></li>
+                <li><a>$$$ - $</a></li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <div
+              class="dropdown dropdown-left"
+              @mouseenter="release = true"
+              @mouseleave="release = false"
+            >
+              <label tabindex="1" class="">Release Date</label>
+              <ul
+                v-if="release"
+                tabindex="1"
+                class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li><a>New to Old</a></li>
+                <li><a>Old to New</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
